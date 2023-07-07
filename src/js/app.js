bc8d88fdf5db844dcc2eddaeac5cef35c4523005
zmv <zmv@mail.com>
2023-07-08 00:07:33 +0400

    rename some unused js

--- src/js/app.js
+++ src/js/app.js
@@ -11,31 +11,8 @@ da.init()
 import { servisesResizer } from "./modules/resizer.js"
 servisesResizer()
 
-// import { hideAndToggle } from "./modules/hider-and-toggler.js"
-// hideAndToggle()
-
 // import { initSpoilers } from "./modules/spoiler.js"
 // document.addEventListener("DOMContentLoaded", initSpoilers)
-
-// const mapFrame = document.querySelector(".location__map iframe")
-// function resizeMap(e) {
-//    if (mapFrame) {
-//       if (document.documentElement.offsetWidth <= 320) {
-//          console.log("<= 320")
-//          if (mapFrame.height !== "200") mapFrame.height = "200"
-//       } else if (document.documentElement.offsetWidth <= 480) {
-//          console.log("<= 480")
-//          if (mapFrame.height !== "300") mapFrame.height = "300"
-//       } else {
-//          console.log("else")
-//          if (mapFrame.height !== "400") mapFrame.height = "400"
-//       }
-//    }
-// }
-// window.addEventListener("resize", resizeMap)
-// document.addEventListener("DOMContentLoaded", resizeMap)
-
-// import "./modules/datepicker.js"
 
 // import { createCustomSelectEl } from "./modules/dynamic-select.js"
 // document.querySelectorAll(".dynamic-select").forEach((el) => createCustomSelectEl(el))
@@ -48,6 +25,3 @@ import { validateSeparateFields, validateWholeForm } from "./modules/form-valida
 const formSelector = ".start-block__form"
 validateWholeForm(formSelector)
 validateSeparateFields(formSelector)
-
-// import { hmm } from "./modules/forms.js"
-// hmm()
